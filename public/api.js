8e2df04d59dd52bd5907cde621435f794af6aac2
pviniciusm <pvinics@gmail.com>
2021-11-11 22:17:11 -0300

    pre deploy

---
+++ public/api.js
@@ -0,0 +1,19 @@
+const api = axios.create({
+    baseURL: "https://api-growdevers-pviniciusm.herokuapp.com",
+});
+
+console.log("teste");
+
+api.get("/")
+    .then((result) => {
+        let array = result.data.data;
+
+        for (let growdever of array) {
+            document.querySelector(
+                "#lista"
+            ).innerHTML += `<li>Nome: ${growdever.nome} - Turma ${growdever.turma}</li>`;
+        }
+    })
+    .catch((err) => {
+        console.log("err");
+    });
